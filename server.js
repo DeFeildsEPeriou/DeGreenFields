24bd402eae8323a32d4e9601920e7ebddf60e139
defields923 <defields923@gmail.com>
2017-02-16 17:44:06 -0600

    add server-logic

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes.js')
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://ericdevin:businessesusersandstuff@ds151059.mlab.com:51059/test-database');
-
 app.use(express.static(path.join(__dirname, '/build')));
 
+mongoose.connect('mongodb://ericdevin:businessesusersandstuff@ds151059.mlab.com:51059/test-database');
 
 app.listen(PORT, () => {
   console.log('listening on 3000')
