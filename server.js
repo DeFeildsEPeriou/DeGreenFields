c75615301968ac9dc75e257b67a6982904ec9329
defields923 <defields923@gmail.com>
2017-02-16 17:29:40 -0600

    add schemas

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var mongoose = require('mongoose');
-let PORT = process.env.PORT || 3000;
-
 mongoose.connect('mongodb://ericdevin:businessesusersandstuff@ds151059.mlab.com:51059/test-database');
 
 app.use(express.static(path.join(__dirname, '/build')));
 
 
-
-
-
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log('listening on 3000')
-})
+});
 
 module.exports = app;
