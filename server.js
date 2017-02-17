75c1e91c5aa25d024958ef24a0e9e7dae4e30d37
defields923 <defields923@gmail.com>
2017-02-16 18:53:54 -0600

    rearrange server logic, modularizing

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const router = require('./routes.js')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require('./server/middleware.js')(app, express);
+require('./server/routes.js')(app, express);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/build')));
-
-mongoose.connect('mongodb://ericdevin:businessesusersandstuff@ds151059.mlab.com:51059/test-database');
+mongoose.connect('mongodb://ericdevin:businessusersstuff@ds153659.mlab.com:53659/whatshappenin');
 
 app.listen(PORT, () => {
   console.log('listening on 3000')
